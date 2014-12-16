ba60f4ff9b2496e577c7f2f3da5b09f757ea4b6b
Alessandro Aglietti <alessandro.aglietti@gmail.com>
2014-12-16 15:14:07 +0100

    file importati

---
+++ cordova/hello/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/accountname/Desktop/anrewapp/hello/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/accountname/Desktop/anrewapp/hello/platforms/android/CordovaLib/AndroidManifest.xml \
